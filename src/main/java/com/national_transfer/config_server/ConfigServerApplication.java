1c5cb0276f1adc47051b5d91e0bcf411db188724
Houssam El aich <elaich.houssam@gmail.com>
2022-02-06 09:13:37 +0100

    Initial commit

---
+++ src/main/java/com/national_transfer/config_server/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.national_transfer.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
